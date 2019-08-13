bd86af1d11daf96345604f6cba197eeaaa8d4fc6
Viet Anh Nguyen <vietanh.phurieng@gmail.com>
2019-08-14 01:04:55 +0700

    edit skills section

--- js/main.js
+++ js/main.js
@@ -151,6 +151,21 @@ $(document).ready(function($) {
 	contentWayPoint();
 
 
+	var skillsDiv = jQuery('#skills');
+
+	jQuery(window).on('scroll', function() {
+	  var winT = jQuery(window).scrollTop(),
+		winH = jQuery(window).height(),
+		skillsT = skillsDiv.offset().top;
+	  if (winT + winH > skillsT) {
+		jQuery('.skillbar').each(function() {
+		  jQuery(this).find('.skillbar-bar').animate({
+			width: jQuery(this).attr('data-percent')
+		  }, 2000);
+		});
+	  }
+	});
+
 	// magnific popup
 	$('.image-popup').magnificPopup({
     type: 'image',
@@ -200,7 +215,6 @@ $(document).ready(function($) {
 
 	};
 	pageProgress();
-
-
 });
+
 
