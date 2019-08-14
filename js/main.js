2e1af6f00393f7e097ed8e1db02bd7f84dd7e028
Viet Anh Nguyen <vietanh.phurieng@gmail.com>
2019-08-15 00:25:14 +0700

    replace new cv

--- js/main.js
+++ js/main.js
@@ -210,10 +210,10 @@ $(document).ready(function($) {
 // 	winH = jQuery(window).height(),
 // 	skillsT = skillsDiv.offset().top;
 //   if (winT + winH > skillsT) {
-// 	jQuery('.skillbar').each(function() {
-// 	  jQuery(this).find('.skillbar-bar').animate({
-// 		width: jQuery(this).attr('data-percent')
-// 	  }, 2000);
+	jQuery('.skillbar').each(function() {
+	  jQuery(this).find('.skillbar-bar').animate({
+		width: jQuery(this).attr('data-percent')
+	  }, 2000);
 // 	});
 //   }
-// });
+});
